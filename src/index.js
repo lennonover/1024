06d81e3b9758a4613c808648dabbadfa9a151590
lennonover <lennonover@163.com>
2017-10-25 17:11:24 +0800

    '增加交互'

--- src/index.js
+++ src/index.js
@@ -3,20 +3,39 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            value:null
+        };
+    }
     render() {
         return (
-        <button className="square">
-            {this.props.value}
+        <button className="square" onClick={() => this.setState({value:"X"})}>
+            {this.state.value}
         </button>
         );
     }
 }
 
 class Board extends React.Component {
-    renderSquare(i) {
-        return <Square value={i} />;
+    constructor(){
+        super();
+        this.state = {
+            squares :Array(9).fill(null),
+        }
     }
-
+    renderSquare(i) {
+        return <Square 
+            value={this.state.squares[i]}
+            onClick = {()=> this.handleClick(i)}
+            />;
+    }
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+      }
     render() {
         const status = 'Next player: X';
 
